00ffd3a08ca46d72211e911fb4345248d0d7c01a
Thomas Trainset <gotoanything@foxmail.com>
2021-08-26 11:16:49 +0800

    test: add test case

---
+++ test/index.ts
@@ -0,0 +1,27 @@
+const jsonTypesGenerator = require('../dist')
+const { promises: fs } = require('fs')
+const { resolve } = require('path')
+
+const getFileName = (name: string) => name.replace(/(\w+).json/, (_, t) => t)
+const formatFileName = (name: string) => name.replace(/-/, '_')
+
+async function main() {
+  const dataDir = resolve('./test', 'data')
+  const dirs: string[] = await fs.readdir(dataDir)
+
+  const dataSource = dirs.map((name: string) => {
+    const fileName = getFileName(name)
+    return {
+      data: require(resolve(dataDir, name)),
+      rootInterfaceName: formatFileName(fileName),
+      outPutPath: resolve('./test', 'output', `${fileName}.types.ts`)
+    }
+  })
+
+  for (const opt of dataSource) {
+    await jsonTypesGenerator.default(opt)
+  }
+}
+
+main()
+
